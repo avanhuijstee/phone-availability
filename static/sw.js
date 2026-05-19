1204b0e31863f6e468e928654d68896922a9e4de
avanhuijstee <aile.vanhuijstee@gupta-strategists.com>
2026-05-19 12:24:29 +0200

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,13 @@
 self.addEventListener('push', (event) => {
   const data = event.data ? event.data.json() : {};
   event.waitUntil(
-    self.registration.showNotification(data.title || '📞 Beschikbaar!', {
-      body: data.body || '',
-      icon: '/static/android-chrome-192x192.png',
+    clients.matchAll({ includeUncontrolled: true, type: 'window' }).then((allClients) => {
+      const appVisible = allClients.some(c => c.visibilityState === 'visible');
+      if (appVisible) return;
+      return self.registration.showNotification(data.title || '📞 Beschikbaar!', {
+        body: data.body || '',
+        icon: '/static/android-chrome-192x192.png',
+      });
     })
   );
 });
